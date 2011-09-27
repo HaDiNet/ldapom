f7852a749bf88afbaec9a3d797463e5535fadb4c
Florian Richter <mail@f1ori.de>
2011-09-27 12:44:30 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ldapom',
+      version='0.9',
+      description='A simple ldap object mapper for python',
+      author='Florian Richter',
+      author_email='mail@f1ori.de',
+      url='https://github.com/f1ori/ldapom',
+      py_modules=['ldapom'],
+     )
