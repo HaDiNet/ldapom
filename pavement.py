ae8a88f42815998335bdd9f8ce5525d6c832197f
Florian Richter <mail@f1ori.de>
2011-11-04 13:22:41 +0100

    release 0.9.2

--- pavement.py
+++ pavement.py
@@ -4,11 +4,11 @@ from paver.easy import task, sh, needs, path
 from paver.setuputils import setup
 
 setup(name='ldapom',
-      version='0.9.1',
+      version='0.9.2',
       description='A simple ldap object mapper for python',
       author='Florian Richter',
       author_email='mail@f1ori.de',
-      url='https://github.com/f1ori/ldapom',
+      url='https://github.com/HaDiNet/ldapom',
       license='MIT',
       keywords = "ldap object mapper",
       long_description=path('README').text(),
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+from distutils.core import setup
+
+# Utility function to read the README file.
+# Used for the long_description. It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='ldapom',
+      version='0.9.2',
+      description='A simple ldap object mapper for python',
+      author='Florian Richter',
+      author_email='mail@f1ori.de',
+      url='https://github.com/HaDiNet/ldapom',
+      license='MIT',
+      keywords = "ldap object mapper",
+      long_description=read('README'),
+      py_modules=['ldapom'],
+     )
+
