5a56b8f3e1ee73b64576eb91d13a9784749f6a09
Leon Handreke <leonh@ndreke.de>
2013-07-17 19:54:34 +0100

    Add more tests

--- tests.py
+++ tests.py
@@ -100,6 +100,33 @@ class LDAPomTest(LDAPServerMixin, unittest.TestCase):
                 "cn=jack,dc=example,dc=com")
         self.assertRaises(AttributeError, getattr, entry, "loginShell")
 
+    def test_modify_attribute(self):
+        entry = self.ldap_connection.get_entry(
+                "cn=jack,dc=example,dc=com")
+        self.assertEqual(entry.loginShell, "/bin/bash")
+        entry.loginShell = "/bin/zsh"
+        entry.save()
+
+        entry = self.ldap_connection.get_entry(
+                "cn=jack,dc=example,dc=com")
+        self.assertEqual(entry.loginShell, "/bin/zsh")
+
+    def test_object_class_getter(self):
+        entry = self.ldap_connection.get_entry(
+                "cn=daniel,dc=example,dc=com")
+        self.assertTrue(entry.is_person)
+        self.assertFalse(entry.is_monkey)
+
+    def test_get_parent(self):
+        entry = self.ldap_connection.get_entry(
+                "cn=daniel,dc=example,dc=com")
+        parent_entry = entry.get_parent()
+        self.assertEqual(parent_entry.dn, "dc=example,dc=com")
+
+    def test_search(self):
+        result = self.ldap_connection.search("cn=*n*")
+        self.assertEqual(set(["Noël", "daniel"]), set([r.dn for r in result]))
+
 
 ## Testcases for ldapom
 class LdapomTest(object):
@@ -116,14 +143,6 @@ class LdapomTest(object):
         self.assertFalse(self.ldap_connection.check_if_dn_exists(s('cn=Noël,dc=example,dc=com')))
         self.assertTrue(self.ldap_connection.check_if_dn_exists(s('cn=Noël2,dc=example,dc=com')))
 
-    ## test delete method
-    def test_delete(self):
-        s = lambda x: self.string_cleaner(x)
-        self.assertTrue(self.ldap_connection.check_if_dn_exists(s('cn=jack,dc=example,dc=com')))
-        node = self.ldap_connection.get_ldap_node(s('cn=jack,dc=example,dc=com'))
-        node.delete()
-        self.assertFalse(self.ldap_connection.check_if_dn_exists(s('cn=jack,dc=example,dc=com')))
-
     ## test check_password method
     def test_check_password(self):
         s = lambda x: self.string_cleaner(x)
@@ -138,108 +157,13 @@ class LdapomTest(object):
         node.set_password(s('asdfä'))
         self.assertTrue(node.check_password(s('asdfä')))
 
-    ## test new_ldap_node method
-    def test_new_node(self):
-        s = lambda x: self.string_cleaner(x)
-        node = self.ldap_connection.new_ldap_node('cn=newuser,dc=example,dc=com')
-        node.objectClass = ['person']
-        node.sn = s('Sören')
-        node.cn = s('newuser')
-        node.save() # the object is created not until here!
-        node = self.ldap_connection.get_ldap_node('cn=newuser,dc=example,dc=com')
-        self.assertEquals([u'Sören'], node.sn)
-        self.assertEquals([u'newuser'], node.cn)
-
-    ## test get ldap node
-    def test_get_ldap_node(self):
-        s = lambda x: self.string_cleaner(x)
-        node = self.ldap_connection.get_ldap_node('cn=jack,dc=example,dc=com')
-        self.assertEquals([u'jack'], node.uid)
-        self.assertEquals(
-            [u'person', u'posixAccount'],
-            [unicode(x) for x in node.objectClass]
-        )
-        # make sure, it's lazy
-        node = self.ldap_connection.get_ldap_node('cn=nobody,dc=example,dc=com')
-        with self.assertRaises(ldap.NO_SUCH_OBJECT):
-            unicode(node.cn)
-
-    def test_retrieve_ldap_node(self):
-        s = lambda x: self.string_cleaner(x)
-        node = self.ldap_connection.retrieve_ldap_node('cn=jack,dc=example,dc=com')
-        self.assertEquals([u'jack'], node.uid)
-        # make sure, it's not lazy
-        with self.assertRaises(ldap.NO_SUCH_OBJECT):
-            node = self.ldap_connection.retrieve_ldap_node('cn=nobody,dc=example,dc=com')
-
-    ## test changing node attributes
-    def test_change_node(self):
-        s = lambda x: self.string_cleaner(x)
-        node = self.ldap_connection.get_ldap_node('cn=jack,dc=example,dc=com')
-        node.sn = s('Sören')
-        node.save()
-        node = self.ldap_connection.get_ldap_node('cn=jack,dc=example,dc=com')
-        self.assertEquals([u'Sören'], node.sn)
-
     ## test searching
     def test_search(self):
         s = lambda x: self.string_cleaner(x)
         result = self.ldap_connection.search(s('cn=*n*'))
         self.assertEqual("[<LdapNode: cn=daniel,dc=example,dc=com>, <LdapNode: cn=Noël,dc=example,dc=com>]", repr(list(result)))
 
-    ## test __str__
-    def test_to_string(self):
-        node = self.ldap_connection.get_ldap_node('cn=Noël,dc=example,dc=com')
-        self.assertEqual("cn=Noël,dc=example,dc=com", str(node))
 
-    ## test __unicode__
-    def test_to_unicode(self):
-        node = self.ldap_connection.get_ldap_node('cn=Noël,dc=example,dc=com')
-        self.assertEqual(u"cn=Noël,dc=example,dc=com", unicode(node))
-
-    ## test __repr__
-    def test_repr(self):
-        node = self.ldap_connection.get_ldap_node('cn=Noël,dc=example,dc=com')
-        self.assertEqual("<LdapNode: cn=Noël,dc=example,dc=com>", repr(node))
-
-    ## test get_parent
-    def test_get_parent(self):
-        node = self.ldap_connection.get_ldap_node('cn=Noël,dc=example,dc=com')
-        parent = node.get_parent()
-        self.assertEqual("<LdapNode: dc=example,dc=com>", repr(parent))
-        self.assertEqual([u"example"], parent.o)
-        self.assertEqual("<LdapNode: dc=com>", repr(parent.get_parent()))
-
-    ## test is_object_class
-    def test_is_object_class(self):
-        node = self.ldap_connection.get_ldap_node('cn=Noël,dc=example,dc=com')
-        self.assertTrue(node.is_posixAccount)
-        self.assertFalse(node.is_windowsAccount)
-
-
-## Testcase ldapom with unicode-strings
-class LdapomUnicodeTest(object):
-    ## decode all input strings to unicode strings
-    def string_cleaner(self, x):
-        return x.decode('utf-8')
-
-
-def set_up(docTest):
-    docTest.ldap_server = openldap.LdapServer()
-    docTest.ldap_server.start()
-    docTest.ldap_connection = ldapom.LdapConnection(uri=docTest.ldap_server.ldapi_url(), base='dc=example,dc=com', login='cn=admin,dc=example,dc=com', password='admin')
-    docTest.globs['ldap_server'] = docTest.ldap_server
-    docTest.globs['ldap_connection'] = docTest.ldap_connection
-    docTest.globs['lc'] = docTest.ldap_connection
-    docTest.globs['jack_node'] = docTest.ldap_connection.get_ldap_node('cn=jack,dc=example,dc=com')
-
-def tear_down(docTest):
-    docTest.ldap_server.stop()
-
-def load_tests(loader, tests, ignore):
-    #tests.addTests(doctest.DocTestSuite(ldapom, setUp=set_up, tearDown=tear_down))
-    #tests.addTests(doctest.DocFileSuite('README', setUp=set_up, tearDown=tear_down))
-    return tests
 
 if __name__ == '__main__':
     unittest.main()
