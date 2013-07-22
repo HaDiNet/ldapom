b03c9598e2f85082210814b3fe743e1ad6aeac25
Leon Handreke <leonh@ndreke.de>
2013-07-22 16:49:10 +0200

    Rename openldap to test_server

--- tests.py
+++ tests.py
@@ -5,13 +5,13 @@ from __future__ import unicode_literals
 import unittest
 
 import ldapom
-import openldap
+import test_server
 
 class LDAPServerMixin(object):
     """Mixin to set up an LDAPConnection connected to a testing LDAP server.
     """
     def setUp(self):
-        self.ldap_server = openldap.LDAPServer()
+        self.ldap_server = test_server.LDAPServer()
         self.ldap_server.start()
         self.ldap_connection = ldapom.LDAPConnection(
                 uri=self.ldap_server.ldapi_url(),
