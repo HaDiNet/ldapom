6bb0bddcd62d4c34678536f0f19e14a0dd81c8c2
Leon Handreke <leonh@ndreke.de>
2013-11-16 12:13:02 +0100

    Move CFFI definitions from connection to a separate module

---
+++ ldapom/cdef.py
@@ -0,0 +1,116 @@
+from cffi import FFI
+
+ffi = FFI()
+
+ffi.cdef("""
+// Type definitions
+typedef ... LDAP;
+typedef ... LDAPMessage;
+typedef ... LDAPControl;
+typedef ... BerElement;
+
+typedef struct ldapmod {
+    int mod_op;
+    char *mod_type;
+    union {
+        char **modv_strvals;
+    //    struct berval **modv_bvals;
+    } mod_vals;
+} LDAPMod;
+
+#define LDAP_VERSION3 ...
+#define LDAP_OPT_PROTOCOL_VERSION ...
+#define LDAP_OPT_X_TLS_REQUIRE_CERT ...
+#define LDAP_OPT_X_TLS_CACERTFILE ...
+#define LDAP_OPT_X_TLS_NEWCTX ...
+#define LDAP_OPT_X_TLS_NEVER ...
+#define LDAP_OPT_TIMELIMIT ...
+#define LDAP_NO_LIMIT ...
+#define LDAP_MOD_ADD ...
+#define LDAP_MOD_DELETE ...
+#define LDAP_MOD_REPLACE ...
+#define LDAP_SCOPE_BASE ...
+#define LDAP_SCOPE_ONELEVEL ...
+#define LDAP_SCOPE_SUBTREE ...
+#define LDAP_SUCCESS ...
+#define LDAP_NO_SUCH_OBJECT ...
+#define LDAP_INVALID_CREDENTIALS ...
+#define LDAP_SERVER_DOWN ...
+
+// Function declarations
+int ldap_initialize(LDAP **ldp, char *uri);
+int ldap_set_option(LDAP *ld, int option, const void *invalue);
+int ldap_simple_bind_s(LDAP *ld, const char *who, const char *passwd);
+int ldap_search_ext_s(
+       LDAP *ld,
+       char *base,
+       int scope,
+       char *filter,
+       char *attrs[],
+       int attrsonly,
+       LDAPControl **serverctrls,
+       LDAPControl **clientctrls,
+       struct timeval *timeout,
+       int sizelimit,
+       LDAPMessage **res);
+
+// From ldap_next_entry(3)
+int ldap_count_entries( LDAP *ld, LDAPMessage *result );
+LDAPMessage *ldap_first_entry( LDAP *ld, LDAPMessage *result );
+LDAPMessage *ldap_next_entry( LDAP *ld, LDAPMessage *entry );
+
+// From ldap_get_values(3)
+char **ldap_get_values(LDAP *ld, LDAPMessage *entry, char *attr);
+int ldap_count_values(char **vals);
+
+// From ldap_get_dn(3)
+char *ldap_get_dn( LDAP *ld, LDAPMessage *entry );
+
+// From ldap_first_attribute(3)
+char *ldap_first_attribute( LDAP *ld, LDAPMessage *entry, BerElement **berptr );
+char *ldap_next_attribute( LDAP *ld, LDAPMessage *entry, BerElement *ber );
+
+// From ldap_add_ext(3)
+int ldap_add_ext_s(
+       LDAP *ld,
+       const char *dn,
+       LDAPMod **attrs,
+       LDAPControl **sctrls,
+       LDAPControl **cctrls );
+
+// From ldap_modify_ext(3)
+int ldap_modify_ext_s(
+              LDAP *ld,
+              char *dn,
+              LDAPMod *mods[],
+              LDAPControl **sctrls,
+              LDAPControl **cctrls );
+
+// From ldap_delete_s(3)
+int ldap_delete_s(LDAP *ld, char *dn);
+
+// From ldap_rename_s(3)
+int ldap_rename_s(
+        LDAP *ld,
+        const char *dn,
+        const char *newrdn,
+        const char *newparent,
+        int deleteoldrdn,
+        LDAPControl *sctrls[],
+        LDAPControl *cctrls[]);
+
+// From ldap_err2string(3)
+char *ldap_err2string( int err );
+
+// From ldap_msgfree(3)
+int ldap_msgfree( LDAPMessage *msg );
+""")
+
+libldap = ffi.verify(
+"""
+// Required for ldap_bind_simple
+#define LDAP_DEPRECATED 1
+
+#include <ldap.h>
+#include <lber.h>
+""", libraries=[str("ldap"), str("lber")])
--- ldapom/connection.py
+++ ldapom/connection.py
@@ -4,136 +4,15 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from cffi import FFI
-
+from ldapom.cdef import libldap, ffi
 from ldapom import attribute
 from ldapom import compat
 from ldapom.entry import LDAPEntry
 from ldapom import error
 
-ffi = FFI()
-
-# Type definitions
-ffi.cdef("""
-typedef ... LDAP;
-typedef ... LDAPMessage;
-typedef ... LDAPControl;
-typedef ... BerElement;
-
-typedef struct ldapmod {
-    int mod_op;
-    char *mod_type;
-    union {
-        char **modv_strvals;
-    //    struct berval **modv_bvals;
-    } mod_vals;
-} LDAPMod;
-""")
-
-ffi.cdef("""
-#define LDAP_VERSION3 ...
-#define LDAP_OPT_PROTOCOL_VERSION ...
-#define LDAP_OPT_X_TLS_REQUIRE_CERT ...
-#define LDAP_OPT_X_TLS_CACERTFILE ...
-#define LDAP_OPT_X_TLS_NEWCTX ...
-#define LDAP_OPT_X_TLS_NEVER ...
-#define LDAP_OPT_TIMELIMIT ...
-#define LDAP_NO_LIMIT ...
-#define LDAP_MOD_ADD ...
-#define LDAP_MOD_DELETE ...
-#define LDAP_MOD_REPLACE ...
-#define LDAP_SCOPE_BASE ...
-#define LDAP_SCOPE_ONELEVEL ...
-#define LDAP_SCOPE_SUBTREE ...
-#define LDAP_SUCCESS ...
-#define LDAP_NO_SUCH_OBJECT ...
-#define LDAP_INVALID_CREDENTIALS ...
-#define LDAP_SERVER_DOWN ...
-""")
-
-# Function declarations
-ffi.cdef("""
-int ldap_initialize(LDAP **ldp, char *uri);
-int ldap_set_option(LDAP *ld, int option, const void *invalue);
-int ldap_simple_bind_s(LDAP *ld, const char *who, const char *passwd);
-int ldap_search_ext_s(
-       LDAP *ld,
-       char *base,
-       int scope,
-       char *filter,
-       char *attrs[],
-       int attrsonly,
-       LDAPControl **serverctrls,
-       LDAPControl **clientctrls,
-       struct timeval *timeout,
-       int sizelimit,
-       LDAPMessage **res);
-
-// From ldap_next_entry(3)
-int ldap_count_entries( LDAP *ld, LDAPMessage *result );
-LDAPMessage *ldap_first_entry( LDAP *ld, LDAPMessage *result );
-LDAPMessage *ldap_next_entry( LDAP *ld, LDAPMessage *entry );
-
-// From ldap_get_values(3)
-char **ldap_get_values(LDAP *ld, LDAPMessage *entry, char *attr);
-int ldap_count_values(char **vals);
-
-// From ldap_get_dn(3)
-char *ldap_get_dn( LDAP *ld, LDAPMessage *entry );
-
-// From ldap_first_attribute(3)
-char *ldap_first_attribute( LDAP *ld, LDAPMessage *entry, BerElement **berptr );
-char *ldap_next_attribute( LDAP *ld, LDAPMessage *entry, BerElement *ber );
-
-// From ldap_add_ext(3)
-int ldap_add_ext_s(
-       LDAP *ld,
-       const char *dn,
-       LDAPMod **attrs,
-       LDAPControl **sctrls,
-       LDAPControl **cctrls );
-
-// From ldap_modify_ext(3)
-int ldap_modify_ext_s(
-              LDAP *ld,
-              char *dn,
-              LDAPMod *mods[],
-              LDAPControl **sctrls,
-              LDAPControl **cctrls );
-
-// From ldap_delete_s(3)
-int ldap_delete_s(LDAP *ld, char *dn);
-
-// From ldap_rename_s(3)
-int ldap_rename_s(
-        LDAP *ld,
-        const char *dn,
-        const char *newrdn,
-        const char *newparent,
-        int deleteoldrdn,
-        LDAPControl *sctrls[],
-        LDAPControl *cctrls[]);
-
-// From ldap_err2string(3)
-char *ldap_err2string( int err );
-
-// From ldap_msgfree(3)
-int ldap_msgfree( LDAPMessage *msg );
-""")
-
-ldap = ffi.verify(
-"""
-// Required for ldap_bind_simple
-#define LDAP_DEPRECATED 1
-
-#include <ldap.h>
-#include <lber.h>
-""", libraries=[str("ldap"), str("lber")])
-
-
-LDAP_SCOPE_BASE = ldap.LDAP_SCOPE_BASE
-LDAP_SCOPE_SUBTREE = ldap.LDAP_SCOPE_SUBTREE
-LDAP_SCOPE_ONELEVEL = ldap.LDAP_SCOPE_ONELEVEL
+LDAP_SCOPE_BASE = libldap.LDAP_SCOPE_BASE
+LDAP_SCOPE_SUBTREE = libldap.LDAP_SCOPE_SUBTREE
+LDAP_SCOPE_ONELEVEL = libldap.LDAP_SCOPE_ONELEVEL
 
 
 def handle_ldap_error(err):
@@ -142,15 +21,15 @@ def handle_ldap_error(err):
     :param err: The error code returned by the library.
     :type err: int
     """
-    if err == ldap.LDAP_SUCCESS:
+    if err == libldap.LDAP_SUCCESS:
         return
 
-    error_string = compat._decode_utf8(ffi.string(ldap.ldap_err2string(err)))
-    if err == ldap.LDAP_NO_SUCH_OBJECT:
+    error_string = compat._decode_utf8(ffi.string(libldap.ldap_err2string(err)))
+    if err == libldap.LDAP_NO_SUCH_OBJECT:
         raise error.LDAPNoSuchObjectError(error_string)
-    elif err == ldap.LDAP_INVALID_CREDENTIALS:
+    elif err == libldap.LDAP_INVALID_CREDENTIALS:
         raise error.LDAPInvalidCredentialsError(error_string)
-    elif err == ldap.LDAP_SERVER_DOWN:
+    elif err == libldap.LDAP_SERVER_DOWN:
         raise error.LDAPServerDownError(error_string)
     else:
         raise error.LDAPError(error_string)
@@ -177,33 +56,33 @@ class LDAPConnection(object):
         self._cacertfile = cacertfile
 
         ld_p = ffi.new("LDAP **")
-        err = ldap.ldap_initialize(ld_p, compat._encode_utf8(uri))
+        err = libldap.ldap_initialize(ld_p, compat._encode_utf8(uri))
         handle_ldap_error(err)
         self._ld = ld_p[0]
 
         version_p = ffi.new("int *")
-        version_p[0] = ldap.LDAP_VERSION3
-        ldap.ldap_set_option(self._ld, ldap.LDAP_OPT_PROTOCOL_VERSION, version_p)
+        version_p[0] = libldap.LDAP_VERSION3
+        libldap.ldap_set_option(self._ld, libldap.LDAP_OPT_PROTOCOL_VERSION, version_p)
 
         if cacertfile:
             require_cert_p = ffi.new("int *")
-            require_cert_p[0] = ldap.LDAP_OPT_X_TLS_NEVER
-            ldap.ldap_set_option(self._ld, ldap.LDAP_OPT_X_TLS_REQUIRE_CERT,
+            require_cert_p[0] = libldap.LDAP_OPT_X_TLS_NEVER
+            libldap.ldap_set_option(self._ld, libldap.LDAP_OPT_X_TLS_REQUIRE_CERT,
                     require_cert_p);
 
-            ldap.ldap_set_option(self._ld, ldap.LDAP_OPT_X_TLS_CACERTFILE,
+            libldap.ldap_set_option(self._ld, libldap.LDAP_OPT_X_TLS_CACERTFILE,
                     compat._encode_utf8(cacertfile))
 
         # For TLS options to take effect, a context refresh seems to be needed.
         newctx_p = ffi.new("int *")
         newctx_p[0] = 0
-        ldap.ldap_set_option(self._ld, ldap.LDAP_OPT_X_TLS_NEWCTX, newctx_p)
+        libldap.ldap_set_option(self._ld, libldap.LDAP_OPT_X_TLS_NEWCTX, newctx_p)
 
         timelimit_p = ffi.new("int *")
         timelimit_p[0] = timelimit
-        ldap.ldap_set_option(self._ld, ldap.LDAP_OPT_TIMELIMIT, timelimit_p)
+        libldap.ldap_set_option(self._ld, libldap.LDAP_OPT_TIMELIMIT, timelimit_p)
 
-        err = ldap.ldap_simple_bind_s(self._ld,
+        err = libldap.ldap_simple_bind_s(self._ld,
                 compat._encode_utf8(bind_dn),
                 compat._encode_utf8(bind_password))
         handle_ldap_error(err)
@@ -213,7 +92,7 @@ class LDAPConnection(object):
     def _fetch_attribute_types(self):
         result = list(
                 self._raw_search(base="cn=subschema",
-                    scope=ldap.LDAP_SCOPE_BASE,
+                    scope=libldap.LDAP_SCOPE_BASE,
                     search_filter="(objectClass=*)",
                     retrieve_attributes=["attributeTypes"]))
         # Decode the type definitions returned to strings
@@ -252,7 +131,7 @@ class LDAPConnection(object):
         return True
 
     def _raw_search(self, search_filter=None, retrieve_attributes=None,
-            base=None, scope=ldap.LDAP_SCOPE_SUBTREE):
+            base=None, scope=libldap.LDAP_SCOPE_SUBTREE):
         """
         Raw wrapper around OpenLDAP ldap_search_ext_s.
 
@@ -282,7 +161,7 @@ class LDAPConnection(object):
         else:
             attrs_p = ffi.NULL
 
-        err = ldap.ldap_search_ext_s(
+        err = libldap.ldap_search_ext_s(
                 self._ld,
                 compat._encode_utf8(base or self._base),
                 scope,
@@ -292,35 +171,35 @@ class LDAPConnection(object):
                 0,
                 ffi.NULL, ffi.NULL,
                 ffi.NULL, # TODO: Implement timeouts
-                0,#ldap.LDAP_NO_LIMIT,
+                0,#libldap.LDAP_NO_LIMIT,
                 search_result_p)
         handle_ldap_error(err)
         search_result = search_result_p[0]
 
-        current_entry = ldap.ldap_first_entry(self._ld, search_result)
+        current_entry = libldap.ldap_first_entry(self._ld, search_result)
         while current_entry != ffi.NULL:
-            dn = ffi.string(ldap.ldap_get_dn(self._ld, current_entry))
+            dn = ffi.string(libldap.ldap_get_dn(self._ld, current_entry))
             attribute_dict = {}
 
             ber_p = ffi.new("BerElement **")
-            current_attribute = ldap.ldap_first_attribute(self._ld,
+            current_attribute = libldap.ldap_first_attribute(self._ld,
                     current_entry, ber_p)
             while current_attribute != ffi.NULL:
                 current_attribute_str = ffi.string(current_attribute)
                 attribute_dict[current_attribute_str] = []
 
-                values_p = ldap.ldap_get_values(self._ld, current_entry,
+                values_p = libldap.ldap_get_values(self._ld, current_entry,
                         current_attribute)
-                for i in range(0, ldap.ldap_count_values(values_p)):
+                for i in range(0, libldap.ldap_count_values(values_p)):
                     attribute_dict[current_attribute_str].append(
                             ffi.string(values_p[i]))
 
-                current_attribute = ldap.ldap_next_attribute(self._ld,
+                current_attribute = libldap.ldap_next_attribute(self._ld,
                         current_entry, ber_p[0])
             # TODO: Call ber_free on ber_p[0]
 
             yield (dn, attribute_dict)
-            current_entry = ldap.ldap_next_entry(self._ld, current_entry)
+            current_entry = libldap.ldap_next_entry(self._ld, current_entry)
             # TODO: Call ldap_msgfree on search_result
 
     def search(self, *args, **kwargs):
@@ -362,7 +241,7 @@ class LDAPConnection(object):
                     scope=LDAP_SCOPE_ONELEVEL)
             for entry in entries_to_delete:
                 entry.delete(recursive=True)
-        err = ldap.ldap_delete_s(self._ld,
+        err = libldap.ldap_delete_s(self._ld,
                 compat._encode_utf8(entry.dn))
         handle_ldap_error(err)
 
@@ -378,7 +257,7 @@ class LDAPConnection(object):
         if new_parent_dn == entry.parent_dn:
             new_parent_dn = None
 
-        err = ldap.ldap_rename_s(self._ld,
+        err = libldap.ldap_rename_s(self._ld,
                 compat._encode_utf8(entry.dn),
                 compat._encode_utf8(new_rdn),
                 (compat._encode_utf8(new_parent_dn)
@@ -435,7 +314,7 @@ class LDAPConnection(object):
             mod = ffi.new("LDAPMod *")
             prevent_garbage_collection.append(mod)
 
-            mod.mod_op = ldap.LDAP_MOD_REPLACE
+            mod.mod_op = libldap.LDAP_MOD_REPLACE
 
             mod_type = ffi.new("char[]", compat._encode_utf8(attribute.name))
             prevent_garbage_collection.append(mod_type)
@@ -454,12 +333,12 @@ class LDAPConnection(object):
         mods[len(save_attributes)] = ffi.NULL
 
         if entry_exists:
-            err = ldap.ldap_modify_ext_s(self._ld,
+            err = libldap.ldap_modify_ext_s(self._ld,
                     compat._encode_utf8(entry.dn),
                     mods,
                     ffi.NULL, ffi.NULL)
         else:
-            err = ldap.ldap_add_ext_s(self._ld,
+            err = libldap.ldap_add_ext_s(self._ld,
                     compat._encode_utf8(entry.dn),
                     mods,
                     ffi.NULL, ffi.NULL)
@@ -475,7 +354,7 @@ class LDAPConnection(object):
         """
         try:
             fetched_entry = next(self.search(base=entry.dn,
-                scope=ldap.LDAP_SCOPE_BASE))
+                scope=libldap.LDAP_SCOPE_BASE))
             entry.attributes = fetched_entry.attributes
             entry._old_attribute_names = set([a.name for a in entry.attributes])
         except StopIteration:
